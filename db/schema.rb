f4dae82525c3fe79057b236dd8dba7d4531e9f53
DylanToporek <dylantoporek@gmail.com>
2022-02-09 13:17:36 -0500

    store resource created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_09_181151) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_09_181640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
