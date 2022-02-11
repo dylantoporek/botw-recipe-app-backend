dc05c6c387b8370812ce36577893b1623b8938ea
DylanToporek <dylantoporek@gmail.com>
2022-02-11 10:41:00 -0500

    ingredient routes / controller updates part 2

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,13 @@
 class IngredientsController < ApplicationController
+
+    def index
+    render json: Ingredient.all, status: :ok
+    end
+
+    def show
+    ingredient = Ingredient.find_by(id: params[:id])
+    render json: ingredient, status: :ok
+    end
+
+    
 end
