84e53933c9892893de1c32ea562f615bbd6df2c6
DylanToporek <dylantoporek@gmail.com>
2022-02-11 17:56:45 -0500

    ingredient seed almost finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ tables2 = html2.css('table')
 materials = tables2[2].css('tr')
 all_materials = materials.map do |row|
     cell_array = row.css('td')
+    binding.pry
     if cell_array.empty? 
         nil
     else
@@ -33,88 +34,12 @@ ingredients = filtered_materials.map do |mat|
     }
 end
 
-# ingredients = filtered_materials.map do |mat|
-#     if filtered_materials.length < 1 
-#         nil
-#     elsif mat[:name].match?(/Apple|Lotus|Durian|Hydromelon|Banana|Fruit|Pepper|Voltfruit|Wildberry/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Fruit" 
-#         }
-#     elsif mat[:name].match?(/Truffle|Chillshroom|Shroom|Ironshroom|Razorshroom|Rushroom|Sunshroom|Zapshrrom/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Mushroom" 
-#         } 
-#     elsif mat[:name].match?(/Radish|Carrot|Pumpkin/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Vegetable" 
-#         }
-#     elsif mat[:name].match?(/Meat/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Red Meat" 
-#         }
-#     elsif mat[:name].match?(/Bird/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Poultry" 
-#         }
-#     elsif mat[:name].match?(/Crab/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Crab"  
-#         }
-#     elsif mat[:name].match?(/Bass|Trout|Porgy|Carp|Salmon/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Fish"  
-#         } 
-#     elsif mat[:name].match?(/Snail/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Snail"
-#         }
-#     elsif mat[:name].match?(/Acorn|Egg|Chickaloo|Honey|Butter|Rice|Spice|Wheat|Milk|Sugar/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Misc" 
-#         }
-#     elsif mat[:name].match?(/Armoranth|Nightshade|Saffina|Herb|Thistle|Princess|Violet/)
-#         {
-#             name: mat[:name],
-#             description: mat[:description],
-#             price: mat[:price],
-#             category: "Herb"
-#         }
-        
-#     end     
-# end
 
 binding.pry
 
 
 
-ingredients.each{|mat| Ingredient.create!(mat)}
+# ingredients.each{|mat| Ingredient.create!(mat)}
 
 
 
