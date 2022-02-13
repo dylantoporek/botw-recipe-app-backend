bfffae11d89bbbd786abae8080a28f0086bb8f8e
DylanToporek <dylantoporek@gmail.com>
2022-02-13 16:29:19 -0500

    login fetch test pass

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   resources :kitchens, only: [:show, :create, :update]
   resources :ingredients, only: [:index, :show]
   resources :stores, only: [:index, :show]
-  resources :users, only: [:create, :show, :update]
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
