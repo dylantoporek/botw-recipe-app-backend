992e02a2caee01759b7999d44e1e7fbc9c4555c3
DylanToporek <dylantoporek@gmail.com>
2022-02-15 18:04:52 -0500

    latest version

--- app/serializers/pantry_serializer.rb
+++ app/serializers/pantry_serializer.rb
@@ -1,5 +1,5 @@
 class PantrySerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :kitchen_id, :ingredient_id
+  attributes :id, :quantity
   has_one :kitchen
   has_one :ingredient
 end
