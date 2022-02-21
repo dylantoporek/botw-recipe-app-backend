342655f5371d303e41c2d87823366764551fe7f4
DylanToporek <dylantoporek@gmail.com>
2022-02-21 11:01:24 -0500

    latest version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
     private
     
     def current_user
-        @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+        @current_user ||= User.find_by(id: session[:user_id])
     end
 
     def authorize
