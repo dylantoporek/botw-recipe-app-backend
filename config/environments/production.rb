c95e489d4f00b6429ec3f9ca6a6714220a341923
DylanToporek <dylantoporek@gmail.com>
2022-03-28 10:35:52 -0400

    client removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_dispatch.cookies_same_site_protection = :None
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
